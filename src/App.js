6e45533693a6f8815c82a36d340598a7449cef7a
Everton <tonmendessilva@gmail.com>
2026-03-21 12:20:32 -0300

    Desafio 3

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import CadastrarAlunos from "./pages/CadastrarAlunos";
-import Alunos from './pages/Alunos';
-import Biblioteca from './pages/Biblioteca';
-import Dashboard from './pages/Dashboard';
-
+import Alunos from "./pages/Alunos";
+import Biblioteca from "./pages/Biblioteca";
+import Dashboard from "./pages/Dashboard";
+import CadastrarLivros from "./pages/CadastrarLivros";
 
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header />
 
-        <Header/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastarAlunos" element={<CadastrarAlunos />} />
+          <Route path="/Alunos" element={<Alunos />} />
+          <Route path="/Biblioteca" element={<Biblioteca />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/cadastrarLivros" element={<CadastrarLivros />} />
+        </Routes>
+      </main>
 
-        <main>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/cadastarAlunos' element={<CadastrarAlunos/>} />
-            <Route path='/Alunos' element={<Alunos/>} />
-            <Route path='/Biblioteca' element={<Biblioteca/>} />
-            <Route path='/Dashboard' element={<Dashboard/>} />
-          </Routes>
-        </main>
-
-        <Footer/>
-
-      </BrowserRouter>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
